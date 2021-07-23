829542986a0bd20b73add3bc438300b255b3e038
Kemzi <kemzi9585@gmail.com>
2021-07-23 11:17:57 +0300

    Added input from keyboard

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'regenerator-runtime/runtime'
 // Calculator variables
 const input = document.querySelector('#input')
 const keys = document.querySelectorAll('.key')
-const panel = document.querySelector('.panel')
+const panel = document.querySelector('#panel')
 let expression = []
 
 
@@ -18,10 +18,67 @@ function calculate() {
       return input.textContent = expression.join('')
    }
 
-   // Clear input content
+   // Reset button function (Clear all)
    function clearInput() {
       expression = []
       input.textContent = '0'
+   }
+
+   // Equal button function (Result)
+
+   function showResult() {
+      if (eval(expression.join('')) === Infinity) {
+         input.textContent = 'Incorrect expression'
+         expression = []
+      } else {
+         // Try to calculate expression
+         try {
+            input.textContent = eval(expression.join(''))
+            expression = [eval(expression.join(''))]
+         } catch(e) {
+            input.textContent = 'Incorrect expression'
+            expression = []
+         }
+      }
+   }
+
+   function keyboardInput(event, eventKey) {
+      let keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', 'Enter', 'Backspace']
+      let symbols = ['+', '-', '/', '*']
+
+      // Checking if key is allowable
+      if (!keys.includes(eventKey)) {
+         return
+      }
+
+      // Checking if key is symbol and adding spaces before and after it
+      if (symbols.includes(eventKey)) {
+         expression.push(' ' + eventKey + ' ')
+         displayInput()
+         return
+      }
+
+      // Enter and backspace key realization
+      if (eventKey === 'Enter') {
+         showResult()
+         return
+      } else if (eventKey === 'Backspace') {
+         deleteLastSymbol()
+         return
+      } 
+
+      expression.push(eventKey)
+      displayInput()
+   }
+
+   // Del button function (Deleting last symbol)
+
+   function deleteLastSymbol() {
+      expression.pop()
+      displayInput()
+      if (expression.length === 0) {
+         input.textContent = '0'
+      }
    }
 
    // Input default value
@@ -30,50 +87,37 @@ function calculate() {
    
    panel.addEventListener('click', event => {
       const el = event.target
-      const elInputAbility = el.dataset.inputable
       const elType = el.dataset.type
       const elValue = el.dataset.value
 
       // Calculator keys
       switch (elValue) {
          case 'del':
-            expression.pop()
-            displayInput()
-            if (expression.length === 0) {
-               input.textContent = '0'
-            }
+            deleteLastSymbol()
             break
          case 'reset': 
             clearInput()
             break
          case '=':
-            // Thow error for dividing on 0
-            if (eval(expression.join('')) === Infinity) {
-               input.textContent = 'Incorrect expression'
-               expression = []
-            } else {
-               // Try to calculate expression
-               try {
-                  input.textContent = eval(expression.join(''))
-                  expression = [eval(expression.join(''))]
-               } catch(e) {
-                  input.textContent = 'Incorrect expression'
-                  expression = []
-               }
-            }
+            showResult()
             break
       }
 
       // Add symbols to input
-      if (elInputAbility && expression.length < 18) {
+      if (el.dataset.inputable && expression.length < 18) {
          // Spaces before and after math sign
          if (elType === 'sign') {
             expression.push(' ' + elValue + ' ')
          } else {
             expression.push(elValue)
          }
+
          displayInput()
       }
+   })
+
+   document.addEventListener('keydown', event => {
+      keyboardInput(event, event.key)
    })
 }
 
